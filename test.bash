9f198baa19d5a6551a700e8e675f6bb0ca977d16
Omura <citrobot5426@gmail.com>
2023-11-28 16:59:05 +0900

    fix test.bash

--- test.bash
+++ test.bash
@@ -2,6 +2,25 @@
 # SPDX-FileCopyrightText: 2023 Junichi Omura
 # SPDX-License-Identifier: BSD-3-Clause
 
-out=$(seq 5 | ./plus)
+ng() {
+     echo NG at Line $1
+     res=1
+}
 
-[ "${out}" = 15 ]
+res=0
+
+### I/O ###
+out=$(seq 5 | ./plus)
+[ "$[out]" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $resi
